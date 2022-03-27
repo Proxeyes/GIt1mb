306f0715fef189de035383000cbf33c05fa9d5a2
Proxeyes <radinom2@mail.ru>
2022-03-27 22:42:19 +0500

    a

--- GIt1mb/Program.cs
+++ GIt1mb/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a;
+int b;
+int c;
+Console.WriteLine("Введите а");
+a = Console.ReadLine();
+
+
+
+
+
+
