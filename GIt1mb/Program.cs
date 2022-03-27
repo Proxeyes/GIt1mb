57781478b72a26180923538bd4d499fd42f9ac5c
Proxeyes <radinom2@mail.ru>
2022-03-27 22:44:13 +0500

    sloj

--- GIt1mb/Program.cs
+++ GIt1mb/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Выберите знак");
+Console.WriteLine("1. +");
+Console.WriteLine("2. -");
+Console.WriteLine("3. :");
+Console.WriteLine("4. *");
+c = Console.ReadLine();
+if (c == 1)
+{
+    Console.WriteLine(a + b);
+
+}
